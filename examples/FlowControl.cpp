513fdbd81b61c7bf4e2b5ef6b9c84c8dc76fa7d5
Casey Kelso <casey@caseykelso.com>
2024-07-29 19:39:47 -0700

    * chamge example to 1.25mbit

--- examples/FlowControl.cpp
+++ examples/FlowControl.cpp
@@ -9,7 +9,7 @@ using namespace mn::CppLinuxSerial;
 int main() {
     // This example relies on a serial device which echos serial data at 9600 baud, 8n1.
     std::cout << __FILE__ << "::" << __func__ << " called." << std::endl;
-    SerialPort serialPort("/dev/ttyACM0", BaudRate::B_9600, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE, HardwareFlowControl::ON, SoftwareFlowControl::OFF);
+    SerialPort serialPort("/dev/ttyACM0", 1250000); //, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE, HardwareFlowControl::ON, SoftwareFlowControl::OFF);
     serialPort.SetTimeout(1000); // Block when reading for 1000ms
 
     serialPort.Open();
@@ -33,7 +33,7 @@ int main() {
         for (int x = 0; x < 10; x++) {
             std::this_thread::sleep_for(100ms);
             std::cout << "Writing \"Hello\"" << std::endl;
-            serialPort.Write("Hello");
+            serialPort.Write("help\n");
         }
     });
 
